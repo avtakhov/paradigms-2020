ddf50ec46629a08d9a94fdd426a0d0af1f4d712e
avtakhov <avtahovfarit@gmail.com>
2020-03-22 23:40:19 +0300

    hw 7 base mod

---
+++ javascript/objectExpression.js
@@ -0,0 +1,76 @@
+"use strict";
+
+function Binary(first, second, apply, strOperation) {
+    this.first = first;
+    this.second = second;
+    this.apply = apply;
+    this.strOperation = strOperation;
+}
+
+Binary.prototype.evaluate = function (x, y, z) {
+    return this.apply(this.first.evaluate(x, y, z), this.second.evaluate(x, y, z));
+};
+
+Binary.prototype.toString = function () {
+    return this.first.toString() + " " + this.second.toString() + " " + this.strOperation;
+};
+
+function Add(first, second) {
+    return new Binary(first, second, (a, b) => a + b, '+');
+}
+
+function Subtract(first, second) {
+    return new Binary(first, second, (a, b) => a - b, '-');
+}
+
+function Multiply(first, second) {
+    return new Binary(first, second, (a, b) => a * b, '*');
+}
+
+function Divide(first, second) {
+    return new Binary(first, second, (a, b) => a / b, '/');
+}
+
+function Unary(first, apply, strOperation) {
+    this.first = first;
+    this.apply = apply;
+    this.strOperation = strOperation;
+}
+
+Unary.prototype.evaluate = function (x, y, z) {
+    return this.apply(this.first.evaluate(x, y, z));
+};
+
+Unary.prototype.toString = function () {
+    return this.first.toString() + " " + this.strOperation;
+};
+
+function Negate(first) {
+    // Unary.call(this, first, x => -x, 'negate');
+    return new Unary(first, x => -x, 'negate');
+}
+
+function Const(c) {
+    this.c = c;
+}
+
+Const.prototype.evaluate = function (x, y, z) {
+    // println(this.c);
+    return this.c;
+};
+
+Const.prototype.toString = function () {
+    return this.c.toString();
+};
+
+function Variable(str) {
+    this.str = str;
+}
+
+Variable.prototype.evaluate = function () {
+    return arguments['xyz'.indexOf(this.str)];
+};
+
+Variable.prototype.toString = function () {
+    return this.str;
+};
